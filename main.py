2ff094b78e7da00a83f62197371fc4deccf7b439
analphagamma <bocz.petya@gmail.com>
2017-10-25 22:22:22 +0100

    Removed secret keys

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ NEWS_SOURCES = {'Magyar Hírlap': 'http://magyarhirlap.hu',
 class TwitterAPI:
     def __init__(self):
         #Authentication data. DO NOT CHANGE!
-        CONSUMER_KEY = "mnfxwU0x7PiF2eSOfWwHCv75V"
-        CONSUMER_SECRET = "zvTqivkeEl1NlHN5sbu9FcCPZ3RWMz6MP9p5vqncdJshTGo4k0"
+        CONSUMER_KEY = ""
+        CONSUMER_SECRET = ""
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        ACCESS_TOKEN = "922910810493603845-tqwv3w5IU7Cpg9R16FfJqYGvtUseu38"
-        ACCESS_TOKEN_SECRET = "wEQVNHqMuMJZGCBqvLnednY6Q5ZX3JuO2e1Yg9JjQjEMn"
+        ACCESS_TOKEN = ""
+        ACCESS_TOKEN_SECRET = ""
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth)
 
